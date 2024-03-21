68073f504cd4f3e8f32106756ea702c12b74f1ca
Zawa_ll <zawalee33@gmail.com>
2024-03-21 09:46:10 -0400

    Gift Model and BlackIP Model Created

--- models/lt_blackip.go
+++ models/lt_blackip.go
@@ -2,8 +2,8 @@ package models
 
 type LtBlackip struct {
 	Id         int    `xorm:"not null pk autoincr INT(10)"`
-	Ip         string `xorm:"not null default '' comment('IP地址') VARCHAR(50)"`
-	Blacktime  int    `xorm:"not null default 0 comment('黑名单限制到期时间') INT(10)"`
-	SysCreated int    `xorm:"not null default 0 comment('创建时间') INT(10)"`
-	SysUpdated int    `xorm:"not null default 0 comment('修改时间') INT(10)"`
+	Ip         string `xorm:"not null default '' comment('IP address') VARCHAR(50)"`
+	Blacktime  int    `xorm:"not null default 0 comment('Blacklist restriction expiration time') INT(10)"`
+	SysCreated int    `xorm:"not null default 0 comment('Creation time') INT(10)"`
+	SysUpdated int    `xorm:"not null default 0 comment('Modify Time') INT(10)"`
 }
--- models/lt_gift.go
+++ models/lt_gift.go
@@ -2,22 +2,22 @@ package models
 
 type LtGift struct {
 	Id           int    `xorm:"not null pk autoincr INT(10)" json:"id"`
-	Title        string `xorm:"not null default '' comment('Gift Name') VARCHAR(255)" json:"title"`
+	Title        string `xorm:"not null default '' comment('Prize name') VARCHAR(255)" json:"title"`
 	PrizeNum     int    `xorm:"not null default -1 comment('Prize quantity, 0 unlimited, >0 limited, <0 no prize') INT(11)" json:"-"`
 	LeftNum      int    `xorm:"not null default 0 comment('Remaining quantity') INT(11)" json:"-"`
-	PrizeCode    string `xorm:"not null default '' comment('0-9999 represents 100% chance, 0-0 represents one in ten thousand chance') VARCHAR(50)" json:"-"`
-	PrizeTime    int    `xorm:"not null default 0 comment('Prize cycle, D days') INT(10)" json:"-"`
+	PrizeCode    string `xorm:"not null default '' comment('0-9999 means 100% chance, 0-0 means one in ten thousand chance of winning') VARCHAR(50)" json:"-"`
+	PrizeTime    int    `xorm:"not null default 0 comment('Award cycle, D days') INT(10)" json:"-"`
 	Img          string `xorm:"not null default '' comment('Prize image') VARCHAR(255)" json:"img"`
-	Displayorder int    `xorm:"not null default 0 comment('Position number, the smaller the number, the earlier the position') INT(10)" json:"displayorder"`
-	Gtype        int    `xorm:"not null default 0 comment('Prize type, 0 virtual currency, 1 virtual coupon, 2 physical item-small, 3 physical item-large') INT(10)" json:"gtype"`
-	Gdata        string `xorm:"not null default '' comment('Extended data, e.g., amount of virtual currency') VARCHAR(255)" json:"-"`
+	Displayorder int    `xorm:"not null default 0 comment('Position number, smaller comes first') INT(10)" json:"displayorder"`
+	Gtype        int    `xorm:"not null default 0 comment('Prize type, 0 virtual coin, 1 virtual voucher, 2 physical-small prize, 3 physical-big prize') INT(10)" json:"gtype"`
+	Gdata        string `xorm:"not null default '' comment('Extended data, e.g., virtual coin amount') VARCHAR(255)" json:"-"`
 	TimeBegin    int    `xorm:"not null default 0 comment('Start time') INT(11)" json:"-"`
 	TimeEnd      int    `xorm:"not null default 0 comment('End time') INT(11)" json:"-"`
-	PrizeData    string `xorm:"comment('Prize distribution plan, [[Time1,Quantity1],[Time2,Quantity2]]') MEDIUMTEXT" json:"-"`
-	PrizeBegin   int    `xorm:"not null default 0 comment('Start of the prize distribution plan cycle') INT(11)" json:"-"`
-	PrizeEnd     int    `xorm:"not null default 0 comment('End of the prize distribution plan cycle') INT(11)" json:"-"`
+	PrizeData    string `xorm:"comment('Award plan, [[Time1, Quantity1],[Time2, Quantity2]]') MEDIUMTEXT" json:"-"`
+	PrizeBegin   int    `xorm:"not null default 0 comment('Start of the award plan cycle') INT(11)" json:"-"`
+	PrizeEnd     int    `xorm:"not null default 0 comment('End of the award plan cycle') INT(11)" json:"-"`
 	SysStatus    int    `xorm:"not null default 0 comment('Status, 0 normal, 1 deleted') SMALLINT(5)" json:"-"`
 	SysCreated   int    `xorm:"not null default 0 comment('Creation time') INT(10)" json:"-"`
-	SysUpdated   int    `xorm:"not null default 0 comment('Update time') INT(10)" json:"-"`
+	SysUpdated   int    `xorm:"not null default 0 comment('Modification time') INT(10)" json:"-"`
 	SysIp        string `xorm:"not null default '' comment('Operator IP') VARCHAR(50)" json:"-"`
 }
